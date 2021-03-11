107127d184f8c0bff06557e93840f55f3411dc47
Mhayk Whandson <hi@mhayk.com>
2021-03-11 17:27:38 +0000

    Repositories have been fetched from the api.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
 
 import '../styles/repositories.scss'
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
     name: 'unform',
@@ -9,14 +12,24 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Repositories list</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {
+                    repositories.map(
+                        repository => <RepositoryItem key={repository.name} repository={repository} />
+                    )
+                }
             </ul>
         </section>
     )
